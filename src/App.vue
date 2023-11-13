37da2b5f534789dbf50a986d5b70ed7c65096e67
mariasolegallina <gallinamariasole@gmail.com>
2023-11-13 12:59:11 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'Boolflix'
+    }
+  }
+}
+</script>
+
+<template>
+  <input type="text" name="" id="">
+  <div>{{ message }}</div>
+</template>
+
+<style lang="scss">
+@use './style/general.scss'
+</style>
