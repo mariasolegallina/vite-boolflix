01eec38daff63a1a451e4f7d9f4a384cd9586e9b
mariasolegallina <gallinamariasole@gmail.com>
2023-11-14 17:18:57 +0100

    add - flags

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -5,6 +5,7 @@ export default {
     data() {
         return {
             store: store,
+            language: ['it', 'es', 'de', 'en', 'fr', 'usa']
         }
     },
     props: {
@@ -18,16 +19,21 @@ export default {
 
 <template>
     <div class="card">
+        <!-- poster -->
         <div class="card__image">
             <img v-if="item.poster_path === null" src="../img/no_poster.png" alt="image_not_found">
             <img v-else :src="`https://image.tmdb.org/t/p/original${item.poster_path}`" alt="poster">
         </div>
+        <!-- testo -->
         <div class="card__text row">
-            <h4 class="title">{{ item.title }}</h4>
-            <h5 class="original-title">{{ item.original_title }}</h5>
+            <h4 class="title">{{ item.title ? item.title : item.name }}</h4>
+            <h5 class="original-title">{{ item.original_title ? item.original_title : original.name }}</h5>
 
-            <!-- vote -->
-            <div>
+            <!-- <img src="https://flagsapi.com/BE/flat/64.png"> -->
+            <img v-if="language.includes(item.original_language)" :src="`/flags/${item.original_language}.png`"
+                alt="original_language">
+            <p v-else>{{ item.original_language }}</p>
+            <div class=" card__vote">
                 <font-awesome-icon v-for="n in Math.round(item.vote_average / 2)" :key="n" icon="fa-solid fa-star" />
                 <font-awesome-icon v-for="n in Math.round(5 - item.vote_average / 2)" :key="n" icon="fa-regular fa-star" />
             </div>
@@ -60,6 +66,10 @@ export default {
         .original-title {
             font-size: 10px;
             font-weight: 400;
+        }
+
+        .vote {
+            font-size: 12px;
         }
     }
 }
