ea48617d1ffb04ae6bb21ad314001da8d378df53
mariasolegallina <gallinamariasole@gmail.com>
2023-11-14 11:39:26 +0100

    add - poster

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -18,8 +18,19 @@ export default {
 
 <template>
     <div class="card">
+        <div class="card__image">
+            <img v-if="item.poster_path === null" src="../img/no_poster.png" alt="image_not_found">
+            <img v-else :src="`https://image.tmdb.org/t/p/original${item.poster_path}`" alt="poster">
+        </div>
         <h4>{{ item.title }}</h4>
         <h5>{{ item.original_title }}</h5>
     </div>
 </template>
-<style lang="scss" scoped></style>
+
+<style lang="scss" scoped>
+.card__image {
+    img {
+        aspect-ratio: 2 / 3;
+    }
+}
+</style>
